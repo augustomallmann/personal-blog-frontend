8d82890c082b35b1b32affe708b86849e13f81bc
Augusto Mallmann <augustocmallmann@gmail.com>
2021-02-06 12:42:56 -0300

    Layout styles

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Profile from '../Profile';
+
+const LayoutWrapper = styled.section`
+  display: flex;
+`;
+
+const LayoutMain = styled.main`
+  background: #16202c;
+  min-height: 100vh;
+  padding: 0 3.75rem 0 20rem;
+  width: 100%;
+`;
+
+const Layout = ({ children }) => (
+  <LayoutWrapper>
+    <aside>
+      <Profile />
+    </aside>
+    <LayoutMain>{children}</LayoutMain>
+  </LayoutWrapper>
+);
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
