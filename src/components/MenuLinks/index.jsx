b42ee61cd03af969d44ad8367e9d829dd8b96371
Augusto Mallmann <augustocmallmann@gmail.com>
2021-02-06 15:12:44 -0300

    MenuLinks Component

---
+++ src/components/MenuLinks/index.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/no-array-index-key */
+import React from 'react';
+import * as S from './styled';
+import links from './content';
+
+function MenuLinks() {
+  return (
+    <S.MenuLinksWrapper>
+      <S.MenuLinksList>
+        {links.map((link, i) => (
+          <S.MenuLinksItem key={i}>
+            <S.MenuLinksLink to={link.url} activeClassName="active">
+              {link.label}
+            </S.MenuLinksLink>
+          </S.MenuLinksItem>
+
+        ))}
+      </S.MenuLinksList>
+    </S.MenuLinksWrapper>
+  );
+}
+
+export default MenuLinks;
