60301d0e66e0f7805021e07d5eb765871114621a
Augusto Mallmann <augustocmallmann@gmail.com>
2021-02-07 16:09:15 -0300

    pagination logic

--- gatsby-node.js
+++ gatsby-node.js
@@ -28,23 +28,49 @@ exports.createPages = ({ graphql, actions }) => {
 
   return graphql(`
    {
-    allMarkdownRemark {
+    allMarkdownRemark(sort:{fields: frontmatter___date, order: DESC}) {
       edges {
         node {
           fields{
             slug
           }
+          frontmatter {
+            background
+            category
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+            description
+            title
+          }
+          timeToRead
         }
       }
     }
   }
   `).then((result) => {
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const posts = result.data.allMarkdownRemark.edges;
+
+    posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
         component: path.resolve('./src/templates/blog-post.jsx'),
         context: {
           slug: node.fields.slug,
+        },
+      });
+    });
+
+    const postsPerPage = 5;
+    const numPages = Math.ceil(posts.length / postsPerPage);
+
+    Array.from({ length: numPages }).forEach((_, index) => {
+      createPage({
+        path: index === 0 ? '/' : `/pagina/${index + 1}`,
+        component: './src/templates/blog-list.jsx',
+        context: {
+          limit: postsPerPage,
+          skip: index * postsPerPage,
+          numPages,
+          currentPage: index + 1,
         },
       });
     });
