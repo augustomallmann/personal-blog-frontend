d8680b43adbc46be5350a4820f5ba3b54178a73e
Augusto Mallmann <augustocmallmann@gmail.com>
2021-02-08 13:28:17 -0300

    format single post with layout and seo components

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -2,18 +2,21 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import SEO from '../components/seo';
 
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark;
 
   return (
-    <>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
       <h1>
         Title:
         {post.frontmatter.title}
       </h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    </>
+    </Layout>
   );
 };
 
