a01a703e977f310dc9189027464d741486982486
Augusto Mallmann <augustocmallmann@gmail.com>
2021-02-09 14:11:26 -0300

    configuring sitemap automation

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,6 +97,8 @@ module.exports = {
         },
       },
     },
+    'gatsby-plugin-sitemap',
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
