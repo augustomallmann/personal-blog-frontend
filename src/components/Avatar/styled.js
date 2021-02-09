c8aeecd46de522fde9f082b4e497eefd5b9282ff
Augusto Mallmann <augustocmallmann@gmail.com>
2021-02-09 10:42:00 -0300

    responsive adapt

--- src/components/Avatar/styled.js
+++ src/components/Avatar/styled.js
@@ -3,8 +3,12 @@ import media from 'styled-media-query';
 import Img from 'gatsby-image';
 
 export const AvatarWrapper = styled(Img)`
-  border-radius:50%;
+  border-radius: 50%;
   height: 3.75rem;
   margin: auto;
   width: 3.75rem;
+  ${media.lessThan('large')`
+    height: 1.875rem;
+    width: 1.875rem;
+  `}
 `;
