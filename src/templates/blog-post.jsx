f0bb5e346cd0d1f33149d77231490ec3ac78afaf
Augusto Mallmann <augustocmallmann@gmail.com>
2021-02-09 14:51:21 -0300

    fix bug regarding images in graphql query

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -16,7 +16,6 @@ const BlogPost = ({ data, pageContext }) => {
       <SEO
         title={post.frontmatter.title}
         description={post.frontmatter.description}
-        image={post.frontmatter.image}
       />
       <S.PostHeader>
         <S.PostDate>
@@ -48,7 +47,6 @@ export const query = graphql`
         title
         description
         date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
-        image
       }
       html
       timeToRead
