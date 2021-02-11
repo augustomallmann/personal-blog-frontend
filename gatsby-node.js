bd6384416e357f4953334f34d4310e1d54590115
Augusto Mallmann <augustocmallmann@gmail.com>
2021-02-11 10:32:01 -0300

    querying pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,70 +27,55 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
 
   return graphql(`
-   {
-    allMarkdownRemark(sort:{fields: frontmatter___date, order: DESC}) {
-      edges {
-        node {
-          fields{
-            slug
-          }
-          frontmatter {
-            background
-            category
-            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
-            description
-            title
-          }
-          timeToRead
-        }
-        next {
-          frontmatter {
-            title
-          }
-          fields {
-            slug
-          }
-        }
-        previous {
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-          }
+  {
+    allStrapiPages{
+      edges{
+        node{
+          Slug
         }
       }
     }
-  }
+    }
   `).then((result) => {
-    const posts = result.data.allMarkdownRemark.edges;
+    // const posts = result.data.allMarkdownRemark.edges;
+    const pages = result.data.allStrapiPages.edges;
 
-    posts.forEach(({ node, next, previous }) => {
+    // posts.forEach(({ node, next, previous }) => {
+    //   createPage({
+    //     path: node.fields.slug,
+    //     component: path.resolve('./src/templates/blog-post.jsx'),
+    //     context: {
+    //       slug: node.fields.slug,
+    //       previousPost: next,
+    //       nextPost: previous,
+    //     },
+    //   });
+    // });
+
+    pages.forEach(({ node }) => {
       createPage({
-        path: node.fields.slug,
-        component: path.resolve('./src/templates/blog-post.jsx'),
+        path: node.Slug,
+        component: path.resolve('./src/templates/page.jsx'),
         context: {
-          slug: node.fields.slug,
-          previousPost: next,
-          nextPost: previous,
+          slug: node.Slug,
         },
       });
     });
 
-    const postsPerPage = 5;
-    const numPages = Math.ceil(posts.length / postsPerPage);
+    // const postsPerPage = 5;
+    // const numPages = Math.ceil(posts.length / postsPerPage);
 
-    Array.from({ length: numPages }).forEach((_, index) => {
-      createPage({
-        path: index === 0 ? '/blog' : `/blog/pagina/${index + 1}`,
-        component: path.resolve('./src/templates/blog-list.jsx'),
-        context: {
-          limit: postsPerPage,
-          skip: index * postsPerPage,
-          numPages,
-          currentPage: index + 1,
-        },
-      });
-    });
+    // Array.from({ length: numPages }).forEach((_, index) => {
+    //   createPage({
+    //     path: index === 0 ? '/blog' : `/blog/pagina/${index + 1}`,
+    //     component: path.resolve('./src/templates/blog-list.jsx'),
+    //     context: {
+    //       limit: postsPerPage,
+    //       skip: index * postsPerPage,
+    //       numPages,
+    //       currentPage: index + 1,
+    //     },
+    //   });
+    // });
   });
 };
