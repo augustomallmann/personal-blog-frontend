ec8f57220c0fe1650d87641bfc8d7582126613d6
Augusto Mallmann <augustocmallmann@gmail.com>
2021-02-12 16:41:34 -0300

    seo upgrade in blog posts

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import Markdown from 'react-markdown';
 import Layout from '../components/Layout';
-// import SEO from '../components/seo';
+import SEO from '../components/seo';
 import * as S from '../components/Posts/styled';
 
 import RecommendedPosts from '../components/RecommendedPosts';
@@ -15,10 +15,10 @@ const BlogPost = ({ data, pageContext }) => {
   const previous = pageContext.previousPost;
   return (
     <Layout>
-      {/* <SEO
+      <SEO
         title={post.Title}
         description={post.SEO.Description}
-      /> */}
+      />
       <S.PostHeader style={{ backgroundImage: ` url(${post.Thumbnail.childImageSharp.fluid.src})` }}>
         <S.Shadow />
         <S.PostDate>
@@ -40,21 +40,32 @@ const BlogPost = ({ data, pageContext }) => {
 
 export const query = graphql`
   query Posts {
-    strapiBlogPosts{
-        Title
-        Data(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
-        Subtitle
-        Content
-        Slug
-        Thumbnail {
+    strapiBlogPosts {
+      Title
+      Data(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+      Subtitle
+      Content
+      Slug
+      SEO {
+        Description
+        OpenGraphImage {
           childImageSharp {
-            fluid(maxWidth: 960) {
+            fluid {
               src
             }
           }
         }
+      }
+      Thumbnail {
+        childImageSharp {
+          fluid(maxWidth: 960) {
+            src
+          }
+        }
+      }
     }
   }
+
 `;
 
 export default BlogPost;
